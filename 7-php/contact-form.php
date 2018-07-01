f7eef90c2db0b86a8ccf13b3a444ca39b82f3067
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-01 15:20:17 -0400

    Fixed contact form title

--- 7-php/contact-form.php
+++ 7-php/contact-form.php
@@ -46,7 +46,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-        <title>Hello, world!</title>
+        <title>Contact Form</title>
     </head>
     <body>
         <div class="container">
