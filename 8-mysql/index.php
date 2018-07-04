3eb1b232a5545518830a3f6a9c5e5d6adec8bbe4
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-03 21:03:22 -0400

    Added secure password storing example

--- 8-mysql/index.php
+++ 8-mysql/index.php
@@ -1,10 +1,13 @@
 <?php
+    // Starting session
+    session_start();
+    
     // Connecting to the database
     $link = mysqli_connect("shareddb-i.hosting.stackcp.net", "users-sujan-333766c3", "nvrkj61pgv", "users-sujan-333766c3");
     if (mysqli_connect_error()) {
         die("Connection failed!");
     }
-
+    
     // Retrieving data
     $query = "SELECT * FROM users";
     if ($result = mysqli_query($link, $query)) {
@@ -29,53 +32,7 @@
         }
     }
 
-    // User form example
-    if (array_key_exists("name", $_POST) && array_key_exists("email", $_POST) && array_key_exists("password", $_POST)
-        && $_POST["name"] != "" && $_POST["email"] != "" && $_POST["password"] != "") {
-        $count = 0;
-        $query = "SELECT `id` FROM users WHERE `email` = '".mysqli_real_escape_string($link, $_POST['email'])."'";
-        $result = mysqli_query($link, $query)
-        if (mysqli_num_rows($result) == 0) {
-            $query = "INSERT INTO users (`name`, `email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['name'])."', '".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
-            if ($success = mysqli_query($link, $query)) {
-                echo "<p>Sign-up successful!</p>";
-            } else {
-                echo "<p>An error occurred while signing up.</p>";
-            }
-        } else {
-            echo "<p>Error: A user with that email already exists!</p>";
-        }
-    } else {
-        echo "<p>One or more fields is empty!</p>";
-    }
+    // Storing passwords securely
+    $row["id"] = 75;
+    echo "Secure password hash: ".md5(md5($row["id"])."password");
 ?>
-
-<html>
-    <head>
-        <title>MySQL</title>
-    </head>
-    <body>
-        <form method="post">
-            <label>Sign-up: </label>
-            <input type="text" name="name" id="name" placeholder="Name">
-            <input type="email" name="email" id="email" placeholder="Email">
-            <input type="password" name="password" id="password" placeholder="Password">
-            <input type="submit">
-        </form>
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        
-        <script>
-            $("form").submit(function(e) {
-                e.preventDefault();
-                if ($("#name").val() == "" || $("#email").val() == "" || $("#password").val() == "") {
-                    alert("One or more fields is empty!");
-                    return false;
-                } else {
-                    $("form").unbind("submit").submit();
-                    return true;
-                }
-            });
-        </script>
-    </body>
-</html>
